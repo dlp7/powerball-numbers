d71003023e1b7508a8feba8897895d62344c352b
David LaPoint <d.r.lapoint@gmail.com>
2017-08-02 21:51:33 -0400

    Initial skeleton code.

---
+++ powerball-numbers.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+
+Entry = namedtuple('Entry', ['name', 'numbers'])
+
+
+def prompt_for_number(person, numbers=None):
+    # TODO: Define loop to prompt for number, check against type, range, and existing.
+    pass
+
+
+def prompt_for_powerball(person, numbers):
+    # TODO: Define loop to prompt for number, check against type and range.
+    pass
+
+
+def create_ticket(entries):
+    # TODO: Loop entries
+    pass
+
+
+if __name__ == '__main__':
+    # TODO: Define main loop
+    pass
