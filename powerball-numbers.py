210e36832015fbaeaf6ded911f3b43b9249a7ad4
David LaPoint <d.r.lapoint@gmail.com>
2017-08-02 22:03:19 -0400

    Added skeleton prompt for generic number, and favorites.

--- powerball-numbers.py
+++ powerball-numbers.py
@@ -3,7 +3,12 @@ from collections import namedtuple
 Entry = namedtuple('Entry', ['name', 'numbers'])
 
 
-def prompt_for_number(person, numbers=None):
+def prompt_for_number(prompttext):
+    # TODO: Define prmpting for actual number
+    pass
+
+
+def prompt_for_favorite(person, numbers=None):
     # TODO: Define loop to prompt for number, check against type, range, and existing.
     pass
 
@@ -15,6 +20,11 @@ def prompt_for_powerball(person, numbers):
 
 def create_ticket(entries):
     # TODO: Loop entries
+    pass
+
+
+def new_entry():
+    # TODO: Prompt for name, numbers
     pass
 
 
